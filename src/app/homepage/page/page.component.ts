525ddd25ca70f8f0eb74c38ed7f383a5028996dc
NhanIsBi <92627042+NhanIsBi@users.noreply.github.com>
2022-10-10 15:39:28 +0700

    feat: page

--- src/app/homepage/page/page.component.ts
+++ src/app/homepage/page/page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
+import { scholarship } from '../model/news.model';
+import { newsService } from '../services/news.service';
 
 @Component({
   selector: 'app-page',
@@ -6,7 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page.component.less'],
 })
 export class PageComponent implements OnInit {
-  constructor() {}
+  listData: scholarship[] = [];
+  rawListScholarship$ = this.sevices.getListScholarship();
+  pageIndex$ = new BehaviorSubject<number>(1);
+  pageSize$ = new BehaviorSubject<number>(5);
+  listData$ = combineLatest({
+    list: this.rawListScholarship$,
+    index: this.pageIndex$,
+    size: this.pageSize$,
+  }).pipe(
+    map(({ list, index, size }) => {
+      return list.slice((index - 1) * size, index * size);
+    })
+  );
+  constructor(private router: Router, private sevices: newsService) {}
 
   ngOnInit(): void {}
+  onPageIndexChange(index: number) {
+    this.pageIndex$.next(index);
+  }
 }
